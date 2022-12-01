34e7da29e271a7dafb06e09610ed01139c46840e
Vlad <vlad14196@gmail.com>
2022-12-01 22:21:22 +0300

    Initial Commit

---
+++ newProject/newProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  newProjectApp.swift
+//  newProject
+//
+//  Created by user on 01.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct newProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
